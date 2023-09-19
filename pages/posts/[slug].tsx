745d8d035f957864f399cc179be44ab3518c8b6b
straxico <straxico@gmail.com>
2023-09-20 01:52:34 +0330

    fix PostHeader

--- pages/posts/[slug].tsx
+++ pages/posts/[slug].tsx
@@ -37,9 +37,7 @@ export default function Post({ post, morePosts, preview }: Props) {
               </Head>
               <PostHeader
                 title={post?.title}
-                coverImage={post?.coverImage}
                 date={post?.date}
-                author={post?.author}
               />
               <PostBody content={post?.content} />
             </article>
